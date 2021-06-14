30d5957e3a83d94523cb73580f9cc8eaded3d00a
JProgrammer <83310250+JProgrammer-it@users.noreply.github.com>
2021-06-15 00:09:21 +0200

    update utils/graphics.py

--- utils/graphics.py
+++ utils/graphics.py
@@ -16,9 +16,11 @@ def print_center(text):
 
 def menu():
     cls()
+    print_center(f"{Fore.BLUE}                  Ethical Hacking Italia")
     print_center(f"{Fore.LIGHTBLACK_EX} █████╗ ██╗██████╗ {Fore.LIGHTRED_EX}███████╗{Fore.YELLOW}██████╗  █████╗ ██████╗ ")
     print_center(f"{Fore.LIGHTBLACK_EX}██╔══██╗██║██╔══██╗{Fore.LIGHTRED_EX}╚══███╔╝{Fore.YELLOW}██╔══██╗██╔══██╗██╔══██╗")
     print_center(f"{Fore.LIGHTBLACK_EX}███████║██║██████╔╝{Fore.LIGHTRED_EX}  ███╔╝ {Fore.YELLOW}██████╔╝███████║██████╔╝")
     print_center(f"{Fore.LIGHTBLACK_EX}██╔══██║██║██╔══██╗{Fore.LIGHTRED_EX} ███╔╝  {Fore.YELLOW}██╔══██╗██╔══██║██╔══██╗")
     print_center(f"{Fore.LIGHTBLACK_EX}██║  ██║██║██║  ██║{Fore.LIGHTRED_EX}███████╗{Fore.YELLOW}██║  ██║██║  ██║██║  ██║")
     print_center(f"{Fore.LIGHTBLACK_EX}╚═╝  ╚═╝╚═╝╚═╝  ╚═╝{Fore.LIGHTRED_EX}╚══════╝{Fore.YELLOW}╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝")
+    print_center(f"         " + f"{Fore.BLACK}{Back.WHITE}JProgrammer-it, C0MPL3XDEV{Style.RESET_ALL}")
